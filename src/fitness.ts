7e40a818fb26ff5a170e542768239cf3605849b1
Elian Cordoba <eliancordoba98@gmail.com>
2022-01-26 19:27:46 -0300

    Rename variable for more readability, also return total weight in fitness function

--- src/fitness.ts
+++ src/fitness.ts
@@ -1,12 +1,12 @@
 import { Fitness, Genome, Item } from "./types";
 
-export function fitness(items: Item[], weight_limit: number, genome: Genome): Fitness {
+export function fitness(items: Item[], weight_limit: number, genome: Genome): [Fitness, number?] {
   if (genome.length !== items.length) {
     throw new Error("Genome and things must be of same length");
   }
 
-  let weight = 0;
-  let value = 0;
+  let total_weight = 0;
+  let total_value = 0;
 
   for (let i = 0; i < genome.length; i++) {
     const gene = genome[i];
@@ -16,13 +16,13 @@ export function fitness(items: Item[], weight_limit: number, genome: Genome): Fi
     }
 
     const thing = items[i];
-    value += thing[1];
-    weight += thing[2];
+    total_value += thing[1];
+    total_weight += thing[2];
 
-    if (weight > weight_limit) {
-      return 0;
+    if (total_weight > weight_limit) {
+      return [0];
     }
   }
 
-  return value;
+  return [total_value, total_weight];
 }
